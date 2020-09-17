83c8ae92a2a509e02628a4ea3a575a4e2d8d445b
Алексей Зобнин <ayuzobnin@edu.hse.ru>
2020-09-17 21:13:59 +0300

    First

---
+++ Task_01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write((char)65);
+        }
+    }
+}
---
+++ Task_06/Program6.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task_06
+{
+    class Program6
+    {
+        static void Main(string[] args)
+        {
+            const double radius = 10.0;
+            do
+            {
+                double posX;
+                do
+                {
+                    Console.WriteLine("Введите абсциссу точки: ");
+                } while (!double.TryParse(Console.ReadLine(), out posX));
+                double posY;
+                do
+                {
+                    Console.WriteLine("Введите ординату точки: ");
+                } while (!double.TryParse(Console.ReadLine(), out posY));
+                if (posX*posX + posY*posY <= radius*radius)
+                {
+                    Console.WriteLine("Точка внутри круга!");
+                }
+                else
+                {
+                    Console.WriteLine("Точка вне круга!");
+                }
+                Console.WriteLine("Нажмите ESC для завершения программы " +
+                    "или любой другой символ для повторного запуска: ");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
